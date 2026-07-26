bcf79be11710bcf46badcb87b5613466f24bda00
Nihalsaeed <34909717+Nihalsaeed@users.noreply.github.com>
2026-07-26 12:44:30 +0530

    Add configuration for user 'nihal' in nix file

---
+++ user-vms/nihal.nix
@@ -0,0 +1,8 @@
+{
+  github = "nihalsaeed";
+  tier = "large";
+  enabled = true;
+  keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPykkjdk3kXfGgRKAllnOse/ez5lU47eGppXuwYmjMH nihalsaeed@gmail.com"
+  ];
+}
