d34c10f2c35e5848264a0844d755205e1cd891c6
l3mon <l3mon@example.com>
2026-07-27 21:41:05 +0530

    member/nihal: add tinker ssh key

--- user-vms/nihal.nix
+++ user-vms/nihal.nix
@@ -3,6 +3,7 @@
   tier = "large";
   enabled = true;
   keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPykkjdk3kXfGgRKAllnOse/ez5lU47eGppXuwYmjMH nihalsaeed@gmail.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKrtVJcz/lEmUGV3csDpeKc8W0B2HXieZeaUBjnWdSz tinker"
   ];
 }
