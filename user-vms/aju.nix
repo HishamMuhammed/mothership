e3380ac9c59f9350379f45fa83cd90cb681aa22a
fahshad <126840635+fahshad-ai@users.noreply.github.com>
2026-07-27 22:13:15 +0530

    Add configuration for user VM 'aju.nix'

---
+++ user-vms/aju.nix
@@ -0,0 +1,8 @@
+{
+  github = "fahshad-ai";
+  tier = "large";
+  enabled = true;
+  keys = [
+       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMBNY5v1oHYzbABmqJQBzUbaV31/8byXRmD9vGHoADPC fahshadaju@gmail.com"
+  ];
+}
