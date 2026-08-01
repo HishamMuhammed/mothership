c291932baf4219fa08906714c2d6c9b87fb046d5
fahshad <126840635+fahshad-ai@users.noreply.github.com>
2026-08-01 18:28:43 +0530

    Delete user-vms/fahshad-test.nix

--- user-vms/fahshad-test.nix
+++
@@ -1,8 +0,0 @@
-{
-  github = "fahshad-ai";
-  tier = "god";
-  enabled = true;
-  keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMBNY5v1oHYzbABmqJQBzUbaV31/8byXRmD9vGHoADPC fahshadaju@gmail.com"
-  ];
-}
